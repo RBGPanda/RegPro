33a140389e946997c8056de4307779d0676ce272
Cole Beck <cbinmobile@outlook.com>
2020-04-17 16:07:41 -0500

    Created the database site.db

--- src/RegProApp.py
+++ src/RegProApp.py
@@ -1,9 +1,19 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
 from Forms import RegistrationForm, LoginForm
-app = Flask(__name__)
 
+app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ef6e3f6a5a0954f0710993da4cd648c6'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(20), unique=False, nullable=False)
+
+    def __repr__(self):
+        return f"User('{ self.username}', '{ self.password }')"
 
 @app.route('/')
 @app.route('/home')
